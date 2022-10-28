361e6f3540cd6c0108bf64f74c92b99e2f236e1c
Jude Oyedele <105249922+Judekennywise@users.noreply.github.com>
2022-10-28 20:18:36 +0100

    added retrieveapi view

--- detailsEndpoint/urls.py
+++ detailsEndpoint/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from .views import DetailsView,DetaislApiView
+from .views import DetailsView, DetailsApiView
 
 urlpatterns = [
     path('', DetailsView.as_view()),
-    path('details/<pk>', DetaislApiView.as_view())
+    path('details/<pk>', DetailsApiView.as_view()),
 ]
